2aa745d9a5f6f4155ac0f00fcd51bbc05f1d0024
Dylan Vann <dylan@dylanvann.com>
2018-08-13 14:37:42 -0400

    Better example.

--- example/index.js
+++ example/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import { FastImage } from "react-fast-image";
 import media from "media.macro";
 
-const App = () => <FastImage {...media("./image.png")} />;
+const App = () => (
+  <div style={{ display: flex, width: "100%" }}>
+    <FastImage {...media("./image-1.png")} />
+    <FastImage {...media("./image-2.png")} />
+    <FastImage {...media("./image-3.png")} />
+  </div>
+);
 
 export default App;
