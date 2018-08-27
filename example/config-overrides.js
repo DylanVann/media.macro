2a0201cc8bbc8b3da47139666c5e0cec91437bce
Dylan Vann <dylan@dylanvann.com>
2018-08-26 21:24:33 -0400

    Setup example.

---
+++ example/config-overrides.js
@@ -0,0 +1,12 @@
+const { injectBabelPlugin } = require('react-app-rewired')
+
+const options = {
+  publicPath: 'static',
+  outputPath: 'public/static',
+}
+
+module.exports = (config, env) => {
+  config = injectBabelPlugin(['media.macro', options], config)
+  return config
+}
+
