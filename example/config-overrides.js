b42b29c4957d2594fe7f1f354ba107d5d5053c7d
Dylan Vann <dylan@dylanvann.com>
2018-08-26 21:43:59 -0400

    Add babel-plugin-macros.

--- example/config-overrides.js
+++ example/config-overrides.js
@@ -6,6 +6,7 @@ const options = {
 }
 
 module.exports = (config, env) => {
+  config = injectBabelPlugin('babel-plugin-macros', config)
   config = injectBabelPlugin(['media.macro', options], config)
   return config
 }
